2c8be7aaaa372b301822e2938f5e9d9f6409c7e8
Felix Wagner <felix.wagner@oeaw.ac.at>
2022-12-19 14:57:05 +0100

    upload files

---
+++ control.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from paho.mqtt import client as mqtt_client
+import numpy as np
+import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pickle
+import os
+import json
+from utils import *
+
+
+# In[2]:
+
+
+broker = 'broker.hivemq.com'
+port = 1883
+client_id = 'control-secondary'
+username = 'fwagner'
+password = '1234'
+
+
+# In[3]:
+
+
+nmbr_channels = 2
+buffer_size = 10
+model_name = 'dummy_agent'
+dac = np.random.uniform(-1, 1, size=nmbr_channels)
+bias_current = np.random.uniform(-1, 1, size=nmbr_channels)
+
+
+# In[4]:
+
+
+# state_memories = [np.memmap('data/state_memory_channel_{}.npy'.format(i), dtype=float, shape=(1000000, 5)) for i in range(nmbr_channels)]
+# next_state_memories = [np.memmap('data/next_state_memory_channel_{}.npy'.format(i), dtype=float, shape=(1000000, 5)) for i in range(nmbr_channels)]
+# action_memories = [np.memmap('data/action_memory_channel_{}.npy'.format(i), dtype=float, shape=(1000000, 2)) for i in range(nmbr_channels)]
+# reward_memories = [np.memmap('data/reward_memory_channel_{}.npy'.format(i), dtype=float, shape=(1000000,)) for i in range(nmbr_channels)]
+# terminal_memories = [np.memmap('data/terminal_memory_channel_{}.npy'.format(i), dtype=float, shape=(1000000,)) for i in range(nmbr_channels)]
+
+ph_memories, rms_memories, memory_idx = [], [], []
+
+for i in range(nmbr_channels):
+    mode = 'r+' if os.path.isfile('data/ph_memory_channel_{}.npy'.format(i)) else 'w+'
+    ph_memories = [np.memmap('data/ph_memory_channel_{}.npy'.format(i), dtype=float, shape=(buffer_size,), mode=mode) for i in range(nmbr_channels)]
+    
+    mode = 'r+' if os.path.isfile('data/rms_memory_channel_{}.npy'.format(i)) else 'w+'
+    rms_memories = [np.memmap('data/rms_memory_channel_{}.npy'.format(i), dtype=float, shape=(buffer_size,), mode=mode) for i in range(nmbr_channels)]
+    
+    mode = 'r+' if os.path.isfile('data/memory_idx_channel_{}.npy'.format(i)) else 'w+'
+    memory_idx = [np.memmap('data/memory_idx_channel_{}.npy'.format(i), dtype=int, shape=1, mode=mode) for i in range(nmbr_channels)]
+
+
+# In[5]:
+
+
+client = connect_mqtt(broker, port, client_id, username, password)
+
+
+# In[6]:
+
+
+for i in range(nmbr_channels):
+    subscribe(client, 'trigger/{}/parameter'.format(i))
+
+
+# In[7]:
+
+
+def receive_and_respond(client, userdata, msg):
+    
+    try:
+        # get data
+        channel = int(msg.topic.split('/')[1])
+        data = json.loads(msg.payload)
+
+        dac[channel] = float(data["DAC"])
+        bias_current[channel] = float(data["BiasCurrent"])
+
+        samples = np.array(data["Samples"], dtype=float)  # TODO convert from Int16
+
+        # calc features
+        offset = np.mean(samples[:25])
+        ph = np.max(samples[25:] - offset)
+        rms = np.std(samples[:25])
+
+        # write to buffer
+        ph_memories[channel][memory_idx[channel][0]] = ph
+        rms_memories[channel][memory_idx[channel][0]] = rms
+        memory_idx[channel][0] += 1
+        memory_idx[channel][0] %= buffer_size
+        ph_memories[channel].flush()
+        rms_memories[channel].flush()
+        memory_idx[channel].flush()
+
+        # plot 
+        # plt.plot(samples)
+        # plt.title('{}, {}'.format(msg.topic, datetime.now()))
+        # plt.xlabel('Sample index')
+        # plt.ylabel('Volt')
+        # plt.show()
+
+        # get new control data
+        with open('models/' + model_name + '_channel_{}.model'.format(channel), 'rb') as handle:
+            policy = pickle.load(handle)
+        state = np.array([dac[channel], bias_current[channel], ph, rms])
+        new_dac, new_bias_current = policy.predict(state)
+
+        # respond
+        print('memory_idx: {}, channel: {}, ph: {}, rms: {}, new dac: {}, new bias current: {}'.format(memory_idx[channel][0], channel, np.max(samples), np.std(samples), new_dac, new_bias_current))
+
+        payload_response = {
+            "ChannelId": channel,
+            "nsTsUTC": time.time(),  # TODO is not in ns
+            "DAC": new_dac, 
+            "BiasCurrent": new_bias_current,
+        }
+
+        result = client.publish('control/{}/set_control'.format(channel), json.dumps(payload_response))
+        check(result)
+        
+    except KeyError as err_msg:
+        print('KeyError: ', err_msg)
+        pass
+
+
+# In[8]:
+
+
+client.on_message = receive_and_respond
+
+
+# In[9]:
+
+
+client.loop_forever()
+
+
+# In[ ]:
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,69 @@
+from paho.mqtt import client as mqtt_client
+import numpy as np
+import time
+from tqdm.auto import tqdm, trange
+import pickle
+
+def connect_mqtt(broker, port, client_id, username, password):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, topic):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+         print(f"Send `{msg}` to topic `{topic}`")
+        else:
+         print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        
+def subscribe(client: mqtt_client, topic):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+    client.subscribe(topic)
+    client.on_message = on_message
+    
+class DummyAgent:
+    
+    def __init__(self, channel, buffer_size):
+        self.channel = channel
+        self.buffer_size = buffer_size
+        self.ph_memory = np.memmap('data/ph_memory_channel_{}.npy'.format(channel), dtype=float, shape=(buffer_size,), mode='r')
+        self.rms_memory = np.memmap('data/rms_memory_channel_{}.npy'.format(channel), dtype=float, shape=(buffer_size,), mode='r')
+        self.memory_idx = np.memmap('data/memory_idx_channel_{}.npy'.format(channel), dtype=int, shape=1, mode='r')
+    
+    def learn(self, steps = 100):
+        for step in trange(steps):
+            
+            batch_idx = np.random.randint(self.buffer_size, size=1)
+            data = np.concatenate([self.ph_memory[batch_idx].reshape(-1,1), 
+                                   self.rms_memory[batch_idx].reshape(-1,1)], axis=1)
+            
+            pass
+    
+    def predict(self, state):
+        return np.random.uniform(-1, 1, size=2)
+    
+    def save(self, path):
+        with open(path, 'wb') as handle:
+            pickle.dump(self, handle)
+            
+def check(result):
+    if result[0] == 0:
+        pass
+    else:
+        print('Message {} not sent!'.format(result[1]))
