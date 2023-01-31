6217a10cfa6122289473f8039b82fb03c259c44d
Felix Wagner <felix.wagner@oeaw.ac.at>
2023-01-31 16:30:24 +0100

    test send

---
+++ test_send.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from paho.mqtt import client as mqtt_client
+import numpy as np
+import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pickle
+import os
+import json
+from utils import *
+
+
+# In[2]:
+
+
+broker = 'localhost'
+port = 1883
+client_id = 'test-primary'
+username = 'fwagner'
+password = '1234'
+
+
+# In[3]:
+
+
+client_sender = connect_mqtt(broker, port, client_id, username, password)
+
+
+# In[4]:
+
+
+test_msg = {'hello': 'world'}
+# test_msg = 'hello world'
+
+
+# In[5]:
+
+
+while True:
+    result = client_sender.publish('ccs/test', json.dumps(test_msg))
+    check(result)
+    time.sleep(5)
+
+
+# In[ ]:
+
+
+
+
